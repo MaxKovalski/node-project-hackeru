99e8c6f13e43309a403036424428ec04af32481e
MaximKovalski <nobesko@gmail.com>
2024-01-24 23:42:11 +0200

    Finish-Joi/change authC

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,9 @@ exports.signup = async (req, res) => {
       const errors = validate.error.details.map((err) => err.message);
       return res.status(403).send(errors);
     }
+
     const hashedPassword = await bcrypt.hash(userData.password, 10);
+    userData.password = hashedPassword;
     const user = new User(userData);
     const savedUser = await user.save();
     res
@@ -31,38 +33,41 @@ exports.signup = async (req, res) => {
   }
 };
 exports.login = async (req, res) => {
-  const { email, password } = req.body;
-  const validate = loginUserValidator.validate(req.body, {
-    abortEarly: false,
-  });
-  if (validate.error) {
-    const errors = validate.error.details.map((err) => err.message);
-    return res.status(403).send(errors);
-  }
-  const user = await User.findOne({ email });
-  if (!user) {
-    return res.status(403).send("email or password in incorrect");
-  }
-  const passwordMatch = await bcrypt.compare(password, user.password);
-  if (!passwordMatch) {
-    return res.status(401).send("email or password in incorrect");
-  }
-  const userObject = user.toObject();
-  delete userObject.password;
-  delete userObject.email;
-  userObject.token = jwt.sign(
-    {
-      userId: userObject._id,
-      isAdmin: userObject.isAdmin,
-      IsBusiness: userObject.IsBusiness,
-    },
-    JWT_SECRET,
-    {
-      expiresIn: "1h",
+  try {
+    const { email, password } = req.body;
+    const validate = loginUserValidator.validate(req.body, {
+      abortEarly: false,
+    });
+    if (validate.error) {
+      const errors = validate.error.details.map((err) => err.message);
+      return res.status(403).send(errors);
     }
-  );
-
-  res.send(userObject.token);
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(403).send("email or password in incorrect");
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).send("email or password in incorrect");
+    }
+    const userObject = user.toObject();
+    delete userObject.password;
+    delete userObject.email;
+    userObject.token = jwt.sign(
+      {
+        userId: userObject._id,
+        isAdmin: userObject.isAdmin,
+        IsBusiness: userObject.IsBusiness,
+      },
+      JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.send(userObject.token);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 exports.hello = (req, res) => {
   res.send("Hello World");
--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,29 +1,102 @@
 const Joi = require("joi");
 const userValidatorSchema = Joi.object({
-  name: Joi.object({
-    first: Joi.string().required(),
-    middle: Joi.string().optional(),
-    last: Joi.string().required(),
-  }),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  name: Joi.object()
+    .keys({
+      first: Joi.string().min(2).max(50).required(),
+      middle: Joi.string().min(2).max(50).allow(""),
+      last: Joi.string().min(2).max(50).required(),
+    })
+    .required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .message(' "email" must be a valid email address')
+    .required(),
+  phone: Joi.string()
+    .pattern(/^(0[2-4,8-9][0-9]{7}|0[57,73,74,76-79]{2}[0-9]{7})$/)
+    .message('user "phone" must be a valid Israeli phone number')
+    .required(),
   address: Joi.object({
-    state: Joi.string(),
-    country: Joi.string(),
-    city: Joi.string(),
-    street: Joi.string(),
-    houseNumber: Joi.number(),
+    state: Joi.string().allow(""),
+    country: Joi.string().required(),
+    city: Joi.string().required(),
+    street: Joi.string().required(),
+    houseNumber: Joi.number().required(),
+    zip: Joi.number(),
   }),
   image: Joi.object({
-    url: Joi.string(),
-    alt: Joi.string(),
+    url: Joi.string()
+      .pattern(/(http(s?):)([/|.|\w|\s|-])*\./)
+      .message('The "url" must be a valid image URL')
+      .required(),
+    alt: Joi.string().min(2).max(50).allow(""),
   }),
 });
 exports.newUserValidator = userValidatorSchema.append({
-  password: Joi.string().required(),
+  password: Joi.string()
+    .pattern(
+      /((?=.*\d{1})(?=.*[A-Z]{1})(?=.*[a-z]{1})(?=.*[!@#$%^&*-]{1}).{7,20})/
+    )
+    .message(
+      '"Password" must be 7-20 characters long and include at least one digit, one uppercase letter, one lowercase letter, and one special character (!@#$%^&*-).'
+    )
+    .required(),
 });
 exports.editUserValidator = userValidatorSchema;
 exports.loginUserValidator = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .message('"email" must be a valid email address')
+    .required(),
+  password: Joi.string()
+    .pattern(
+      /((?=.*\d{1})(?=.*[A-Z]{1})(?=.*[a-z]{1})(?=.*[!@#$%^&*-]{1}).{7,20})/
+    )
+    .message(
+      '"Password" must be 7-20 characters long and include at least one digit, one uppercase letter, one lowercase letter, and one special character (!@#$%^&*-).'
+    )
+    .required(),
 });
+// cardValidator = Joi.object({
+//   title: Joi.string().required().min(2).max(100),
+
+//   subtitle: Joi.string().required().min(2).max(100),
+
+//   description: Joi.string().required().min(10).max(500),
+
+//   phone: Joi.string()
+//   .regex(/0[0-9]{1,2}\-?\s?[0-9]{3}\s?[0-9]{4}/)
+//     .rule({ message: 'card "phone" must be a valid phone number' })
+//     .required(),
+
+//   email: Joi.string()
+//    .pattern(
+//       /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+//     )
+//     .rule({ message: 'card "mail" must be a valid mail' })
+//     .required(),
+
+//   web: Joi.string()
+//     .ruleset.regex(urlRegex)
+//     .rule({ message: 'card "web" must be a valid url' })
+//     .allow(""),
+
+//   image: Joi.object()
+//     .keys({
+//       url: Joi.string()
+//         .ruleset.regex(urlRegex)
+//         .rule({ message: 'card.image "url" must be a valid url' })
+//         .allow(""),
+//       alt: Joi.string().min(2).max(256).allow(""),
+//     })
+//     .required(),
+//   address: Joi.object()
+//     .keys({
+//       state: Joi.string().allow(""),
+//       country: Joi.string().min(2).max(256).required(),
+//       city: Joi.string().min(2).max(256).required(),
+//       street: Joi.string().min(2).max(256).required(),
+//       houseNumber: Joi.number().required(),
+//       zip: Joi.number(),
+//     })
+//     .required(),
+// });
